5dd196f39d8d54c967aa668a9f4d308f64325371
Felipe Grisales Cardona <felipe-grisales@hotmail.com>
2024-04-02 22:36:34 -0500

    CSS básico terminado, creación del documento Javascript y creación de variables.

---
+++ functions.js
@@ -0,0 +1,4 @@
+let catchedInput = document.querySelector(".inputText");
+let buttonEncrypt = document.querySelector(".butEncrypt");
+let buttonDeencrypt = document.querySelector(".butDencrypt");
+let buttonCopy = document.querySelector("#copyButton");
